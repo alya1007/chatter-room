a43699ee3d3f55b135fa305328e0d8610d131144
alya1007 <alexandra.konjevic@isa.utm.md>
2024-10-07 17:29:49 +0300

    read env vars

--- src/ChatService/Program.cs
+++ src/ChatService/Program.cs
@@ -1,12 +1,20 @@
 using ChatService.Services;
+using ChatService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+string connectionString = builder.Configuration["CustomSettings:connectionString"] ?? "";
+string databaseName = builder.Configuration["CustomSettings:databaseName"] ?? "";
+
 builder.Services.AddGrpc();
+
+builder.Services.AddSingleton<ChatServiceDbContext>(new ChatServiceDbContext(connectionString, databaseName));
 
 var app = builder.Build();
 
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ChatManagementService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
