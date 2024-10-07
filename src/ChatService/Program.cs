8a4565189c9ff5f25ba33b995e17db6287fddea7
alya1007 <alexandra.konjevic@isa.utm.md>
2024-10-07 19:47:04 +0300

    add grpc health check

--- src/ChatService/Program.cs
+++ src/ChatService/Program.cs
@@ -1,5 +1,6 @@
 using ChatService.Services;
 using ChatService.Data;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +11,15 @@ string databaseName = builder.Configuration["CustomSettings:databaseName"] ?? ""
 
 builder.Services.AddGrpc();
 
+builder.Services.AddGrpcHealthChecks().AddCheck("Server", () => HealthCheckResult.Healthy());
+
 builder.Services.AddSingleton<ChatServiceDbContext>(new ChatServiceDbContext(connectionString, databaseName));
 
 var app = builder.Build();
 
 app.MapGrpcService<ChatManagementService>();
+app.MapGrpcHealthChecksService()
+    .AllowAnonymous();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
--- src/UserService/Program.cs
+++ src/UserService/Program.cs
@@ -1,5 +1,6 @@
 using UserService.Services;
 using UserService.Data;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +12,15 @@ string databaseName = builder.Configuration["CustomSettings:databaseName"] ?? ""
 
 builder.Services.AddGrpc();
 
+builder.Services.AddGrpcHealthChecks().AddCheck("Server", () => HealthCheckResult.Healthy());
+
 builder.Services.AddSingleton<UserServiceDbContext>(new UserServiceDbContext(connectionString, databaseName));
 
 var app = builder.Build();
 
 app.MapGrpcService<UserManagementService>();
+app.MapGrpcHealthChecksService()
+    .AllowAnonymous();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
