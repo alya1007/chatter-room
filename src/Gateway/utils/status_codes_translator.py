cc595aa24c8c11a97e516a6859c631343c89061d
alya1007 <alexandra.konjevic@isa.utm.md>
2024-10-15 22:06:52 +0300

    add translator from grpc status codes to http

---
+++ src/Gateway/utils/status_codes_translator.py
@@ -0,0 +1,18 @@
+import grpc # type: ignore
+
+
+grpc_to_http_status_codes = {
+    grpc.StatusCode.INVALID_ARGUMENT: 400,
+    grpc.StatusCode.NOT_FOUND: 404,
+    grpc.StatusCode.ALREADY_EXISTS: 409,
+    grpc.StatusCode.PERMISSION_DENIED: 403,
+    grpc.StatusCode.UNAUTHENTICATED: 401,
+    grpc.StatusCode.UNIMPLEMENTED: 501,
+    grpc.StatusCode.INTERNAL: 500
+}
+
+
+def grpc_status_to_http(status: grpc.StatusCode) -> int:
+    return grpc_to_http_status_codes.get(status, 500)
+
+
