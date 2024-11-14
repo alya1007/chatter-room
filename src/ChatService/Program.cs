1b7a08a754ba0221c619cb1484d7d8c884fa984b
alya1007 <alexandra.konjevic@isa.utm.md>
2024-11-14 23:37:43 +0200

    assign dynamic port chat service

--- src/ChatService/Program.cs
+++ src/ChatService/Program.cs
@@ -4,8 +4,24 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+
+int grpcPort = int.Parse(Environment.GetEnvironmentVariable("GRPC_PORT") ?? "11000");
+int webSocketPort = int.Parse(Environment.GetEnvironmentVariable("WEBSOCKET_PORT") ?? "11001");
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(grpcPort, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+    options.ListenAnyIP(webSocketPort, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+});
 
 string connectionString = Environment.GetEnvironmentVariable("CHAT_CONNECTION_STRING") ?? "";
 string databaseName = Environment.GetEnvironmentVariable("CHAT_DATABASE_NAME") ?? "";
